a6c263f2acc94ff1ebdd913a5c5256756e4551e2
Natalia Nazarczuk <01151472@pw.edu.pl>
2022-04-16 21:48:08 +0200

    Add project files.

---
+++ WpfBinding/Converters.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfBinding
+{
+    class GetFlightsList : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Plane))
+            {
+                return null;
+            }
+
+            return ((Plane)value).Flights;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class GetWeight : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Plane))
+            {
+                return null;
+            }
+
+            return ((Plane)value).Weight*1000;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class GetArrival : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Flight))
+            {
+                return null;
+            }
+
+            DateTime departure = ((Flight)value).DepartureTime;
+            TimeSpan time = ((Flight)value).Duration;
+            DateTime arrival = departure.Add(time);
+
+            return arrival;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
